edbd3962ee1ab1aa4aa441c5df46ae16369dded2
Kenneth Dait <kendaitdev@icloud.com>
2023-12-21 06:12:31 -0600

    added boilerplate code in typescript

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,19 @@
  *
  */
 
-
 function greet(person: string): string {
 	console.log(`Hello, ${person}!`);
+}
+
+/**
+ * handle HTTP GET requests
+ * 
+ * @returns {GoogleAppsScript.HTML.HtmlOutput}
+ */
+function doGet(e) {
+  e = e || {};
+  const template = HtmlService.createTemplate('<h1>Hello world!</h1><pre><?= data ?></pre>');
+  template.data = JSON.stringify(e);
+  return template.evaluate()
+    .setTitle('sandbox-dev');
 }
